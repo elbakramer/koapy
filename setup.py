b892ec7384f324d4774c637f9ea7c6c1354032e7
Yunseong Hwang <kika1492@gmail.com>
2021-03-29 08:40:06 +0900

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     packages=find_packages(include=['koapy', 'koapy.*']),
     test_suite='tests',
     url='https://github.com/elbakramer/koapy',
-    version='0.3.3',
+    version='0.3.4',
     zip_safe=False,
 )
