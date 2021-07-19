db76aad04edf05f4fc10c0a1679b30b23310c21b
Yunseong Hwang <kika1492@gmail.com>
2021-07-19 12:56:53 +0900

    Deal with cache_ok related warning

--- koapy/utils/store/sqlalchemy/Timestamp.py
+++ koapy/utils/store/sqlalchemy/Timestamp.py
@@ -10,6 +10,8 @@ from sqlalchemy.types import TypeDecorator
 class Timestamp(TypeDecorator):  # pylint: disable=abstract-method
 
     impl = DateTime
+    cache_ok = True
+
     signature = inspect.signature(impl)
 
     utc = datetime.timezone.utc
