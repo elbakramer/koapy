c2f89a9efbf2e548a79c58d44c72770db5a95823
Yunseong Hwang <kika1492@gmail.com>
2021-07-20 06:42:05 +0900

    Improve Logging

--- koapy/utils/logging/Logging.py
+++ koapy/utils/logging/Logging.py
@@ -6,10 +6,10 @@ import threading
 
 class LoggingMeta(type):
 
-    __config = {
+    __default_config = {
         "version": 1,
         "formatters": {
-            "basic": {
+            "default": {
                 "format": "%(asctime)s [%(levelname)s] %(message)s - %(filename)s:%(lineno)d",
             },
         },
@@ -17,7 +17,7 @@ class LoggingMeta(type):
             "console": {
                 "class": "logging.StreamHandler",
                 "level": "NOTSET",
-                "formatter": "basic",
+                "formatter": "default",
             },
         },
         "loggers": {
@@ -31,6 +31,8 @@ class LoggingMeta(type):
         "disable_existing_loggers": False,
     }
 
+    __config_key = "koapy.utils.logging.config"
+
     __initialized = False
     __init_lock = threading.RLock()
 
@@ -38,10 +40,17 @@ class LoggingMeta(type):
         if not LoggingMeta.__initialized:
             with LoggingMeta.__init_lock:
                 if not LoggingMeta.__initialized:
-                    return LoggingMeta.__initialize(cls)
+                    LoggingMeta.__initialize(cls)
 
     def __initialize(cls):
-        logging.config.dictConfig(LoggingMeta.__config)
+        from koapy.config import config
+
+        logging_config_dict = config.get(
+            LoggingMeta.__config_key, LoggingMeta.__default_config
+        )
+
+        logging.basicConfig()
+        logging.config.dictConfig(logging_config_dict)
         LoggingMeta.__initialized = True
 
     def __new__(cls, clsname, bases, dct):
@@ -51,45 +60,85 @@ class LoggingMeta(type):
         super().__init__(clsname, bases, dct)
         cls.__logger = None
 
+    def __get_name_from_module(cls, module):
+        module_name = module.__name__
+        module_spec = module.__spec__
+        if module_spec is not None:
+            module_name = module_spec.name
+        return module_name
+
+    def __module_name(cls):
+        module = inspect.getmodule(cls)
+        module_name = LoggingMeta.__get_name_from_module(cls, module)
+        return module_name
+
     def __class_name(cls):
         class_name = cls.__name__
         if hasattr(cls, "__outer_class__"):
-            class_name = "%s.%s" % (
+            class_name = "{}.{}".format(
                 LoggingMeta.__class_name(cls.__outer_class__),
                 class_name,
             )
         return class_name
 
     def __logger_name(cls):
-        module = inspect.getmodule(cls)
-        module_spec = module.__spec__
-        if module_spec is not None:
-            module_name = module_spec.name
-        else:
-            module_name = module.__name__
+        module_name = LoggingMeta.__module_name(cls)
         class_name = LoggingMeta.__class_name(cls)
-        logger_name = "%s.%s" % (module_name, class_name)
+        logger_name = "{}.{}".format(module_name, class_name)
         return logger_name
 
-    @property
-    def logger(cls):
+    def _get_logger(cls):
         if cls.__logger is None:
             LoggingMeta.__initialize_if_necessary(cls)
             logger_name = LoggingMeta.__logger_name(cls)
             cls.__logger = logging.getLogger(logger_name)
         return cls.__logger
 
+    @property
+    def logger(cls):
+        return LoggingMeta._get_logger(cls)
+
     def get_logger(cls, name=None):
         if name is None:
-            name = LoggingMeta.__logger_name(cls)
-            if name == "__main__":
-                name = "koapy"
+            caller_frameinfo = inspect.stack()[1]
+            caller_module = inspect.getmodule(caller_frameinfo.frame)
+            name = LoggingMeta.__get_name_from_module(cls, caller_module)
         LoggingMeta.__initialize_if_necessary(cls)
         logger = logging.getLogger(name)
         return logger
+
+    def verbosity_to_loglevel(cls, verbosity=0):
+        verbosity = verbosity or 0
+        levels = [
+            logging.WARNING,
+            logging.INFO,
+            logging.DEBUG,
+        ]
+        if verbosity >= len(levels):
+            verbosity = -1
+        level = levels[verbosity]
+        return level
+
+    def loglevel_to_verbosity(cls, loglevel=logging.WARNING):
+        verbosity = 3 - (loglevel + 9) // 10
+        if verbosity < 0:
+            verbosity = 0
+        return verbosity
+
+    def set_loglevel(cls, loglevel=logging.WARNING, logger=None):
+        if logger is None:
+            logger = "koapy"
+        return logging.getLogger(logger).setLevel(loglevel)
+
+    def set_verbosity(cls, verbosity=0, logger=None):
+        if logger is None:
+            logger = "koapy"
+        loglevel = LoggingMeta.verbosity_to_loglevel(cls, verbosity)
+        LoggingMeta.set_loglevel(cls, loglevel, logger)
+        return loglevel
 
 
 class Logging(metaclass=LoggingMeta):
     @property
     def logger(self):
-        return self.__class__.logger
+        return self.__class__._get_logger()
--- koapy/utils/logging/__init__.py
+++ koapy/utils/logging/__init__.py
@@ -1,54 +1,11 @@
 import inspect
-import logging
 
 from koapy.utils.logging.Logging import Logging
 
+get_logger = Logging.get_logger
 
-def verbosity_to_loglevel(verbosity=0):
-    verbosity = verbosity or 0
-    levels = [
-        logging.WARNING,
-        logging.INFO,
-        logging.DEBUG,
-    ]
-    if verbosity >= len(levels):
-        verbosity = -1
-    level = levels[verbosity]
-    return level
+verbosity_to_loglevel = Logging.verbosity_to_loglevel
+loglevel_to_verbosity = Logging.loglevel_to_verbosity
 
-
-def loglevel_to_verbosity(loglevel=logging.WARNING):
-    verbosity = 3 - (loglevel + 9) // 10
-    if verbosity < 0:
-        verbosity = 0
-    return verbosity
-
-
-def set_loglevel(loglevel=logging.WARNING, logger=None):
-    if logger is None:
-        logger = "koapy"
-    return logging.getLogger(logger).setLevel(loglevel)
-
-
-def set_verbosity(verbosity=0, logger=None):
-    if logger is None:
-        logger = "koapy"
-    loglevel = verbosity_to_loglevel(verbosity)
-    set_loglevel(loglevel, logger)
-    return loglevel
-
-
-def get_module_name(offset=0):
-    frame_infos = inspect.stack()
-    frame = frame_infos[1 + offset]
-    module = inspect.getmodule(frame[0])
-    module_name = module.__name__
-    return module_name
-
-
-def get_logger(name=None):
-    if name is None:
-        module_name = get_module_name(1)
-        name = module_name
-    logger = logging.getLogger(name)
-    return logger
+set_loglevel = Logging.set_loglevel
+set_verbosity = Logging.set_verbosity
