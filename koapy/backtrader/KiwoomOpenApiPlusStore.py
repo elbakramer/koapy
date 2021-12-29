d67aace7a5d07ec8fed46b089908360ffc5a5d58
resoliwan <resoliwan@naver.com>
2021-12-29 14:47:42 +0900

    [BUGFIX] order event receive error. 가끔 이벤트 수신 순서가 바뀌는 경우가 존재. 키움 명세서에도 이벤트 순서가 바뀔 수 있다고 명세 되어 있습니다.

--- koapy/backtrader/KiwoomOpenApiPlusStore.py
+++ koapy/backtrader/KiwoomOpenApiPlusStore.py
@@ -211,9 +211,17 @@ class API:
             quote_type,
             original_order_no,
         )
-        _msg = next(responses)
-        _tr = next(responses)
-        accept = next(responses)
+
+        # 이벤트 수신 순서가 바뀌는 경우가 존재
+        res = [next(responses) for _ in range(3)]
+        ORDER_RES_SEQ = {
+            "OnReceiveMsg": 1,
+            "OnReceiveTrData": 2,
+            "OnReceiveChejanData": 3,
+        }
+        res.sort(key=lambda r: ORDER_RES_SEQ[r.name])
+        _msg, _tr, accept = res
+
         accept_data = dict(zip(accept.single_data.names, accept.single_data.values))
         result = {"orderOpened": {"id": accept_data["주문번호"]}}
         return result
