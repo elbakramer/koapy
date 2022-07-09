e3787d171a20615550ecfb433e5feed3fa92d620
Yunseong Hwang <kika1492@gmail.com>
2022-07-10 07:21:24 +0900

    Bump version: 0.8.3 → 0.8.4

--- setup.py
+++ setup.py
@@ -110,6 +110,6 @@ setup(
     include_package_data=True,
     test_suite="tests",
     url="https://github.com/elbakramer/koapy",
-    version="0.8.3",
+    version="0.8.4",
     zip_safe=False,
 )
